6058a13f67733d1bdb3bc770b601a866cd3f0a14
Raineriojr <raijunior20@gmail.com>
2026-02-02 17:59:34 -0300

    feat: hero section

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,22 @@ import Contact from "@/components/Contact";
 export default function Home() {
   return (
     <>
-      <section id="início">
+      <section id="início" className="min-h-screen flex flex-col">
         <Hero />
+
+        <div className="mt-auto">
+          <TechStack />
+        </div>
       </section>
-      
-      <TechStack />
-      
+
       <section id="sobre">
         <About />
       </section>
-      
+
       <section id="projetos">
         <Projects />
       </section>
-      
+
       <section id="contato">
         <Contact />
       </section>
