cb182c8e9694a1a2aaae00df312f8a01d980eb9a
Raineriojr <raijunior20@gmail.com>
2026-02-04 16:06:20 -0300

    fix: email change

--- constants/contacts.tsx
+++ constants/contacts.tsx
@@ -2,7 +2,19 @@ import { SiGithub } from "@icons-pack/react-simple-icons";
 import { Linkedin, Mail } from "lucide-react";
 
 export const contacts = [
-    { icon: <SiGithub className="w-6 h-6" />, label: "GitHub", link: "https://github.com/Raineriojr" },
-    { icon: <Linkedin className="w-6 h-6" />, label: "LinkedIn", link: "https://www.linkedin.com/in/rainerio-costa/" },
-    { icon: <Mail className="w-6 h-6" />, label: "E-mail", link: "mailto:raijunior20@gmail.com" },
-  ];
+  {
+    icon: <SiGithub className="w-6 h-6" />,
+    label: "GitHub",
+    link: "https://github.com/Raineriojr",
+  },
+  {
+    icon: <Linkedin className="w-6 h-6" />,
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/rainerio-costa/",
+  },
+  {
+    icon: <Mail className="w-6 h-6" />,
+    label: "E-mail",
+    link: "https://mail.google.com/mail/?view=cm&fs=1&to=raijunior20@gmail.com",
+  },
+];
